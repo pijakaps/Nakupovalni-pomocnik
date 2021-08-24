5f9b1169f7c15a69fc979a592e8564327b0feafe
Pija <pija.kaps@gmail.com>
2021-08-24 15:59:29 +0200

    manjši popravki modela

--- model.py
+++ model.py
@@ -22,8 +22,14 @@ class Stanje:
     def dodaj_izdelek(self, izdelek):
         self.aktualna_kategorija.dodaj_izdelek(izdelek)
 
-    def pobrisi_opravilo(self, izdelek):
+    def pobrisi_izdelek(self, izdelek):
         self.aktualna_kategorija.izbrisi_izdelek(izdelek)
+
+    def potrebujem_izdelek(self, izdelek):
+        self.aktualna_kategorija.potrebujem_izdelek(izdelek)
+    
+    def kupi_izdelek(self, izdelek):
+        self.aktualna_kategorija.kupi_izdelek(izdelek)
 
     def dodaj_nakup(self, nakup):
         self.nakupi.append(nakup)
@@ -37,7 +43,7 @@ class Stanje:
         self.aktualni_nakup = nakup
 
     def zabelezi_izdelek(self, izdelek):
-        self.aktualni_nakup.dodaj_izdelek(izdelek)
+        self.aktualni_nakup.zabelezi_izdelek(izdelek)
 
 
 class Nakup:
@@ -51,7 +57,7 @@ class Nakup:
     def strosek(self):
         skupaj = 0
         for izdelek in self.kupljeni_izdelki:
-            skupaj += izdelek.strosek_izdelka
+            skupaj += izdelek.strosek_izdelka()
         return skupaj
 
     def zabelezi_izdelek(self, izdelek):
@@ -61,46 +67,36 @@ class Nakup:
 class Kategorija:
     def __init__(self, ime):
         self.ime = ime
-        self.spisek_izdelkov = []
-        self.potrebujem_izdelke = []
+        self.izdelki = []
+        self.potrebujem =[]
 
     def __repr__(self):
         return f"Kategorija({self.ime})"
 
     def dodaj_izdelek(self, izdelek):
-        self.spisek_izdelkov.append(izdelek)
+        self.izdelki.append(izdelek)
 
     def izbrisi_izdelek(self, izdelek):
-        self.spisek_izdelkov.remove(izdelek)
+        self.izdelki.remove(izdelek)
+    
+    def potrebujem_izdelek(self, izdelek):
+        self.potrebujem.append(izdelek)
+    
+    def kupi_izdelek(self, izdelek):
+        self.potrebujem.remove(izdelek)
 
-    def potrebujem(self, izdelek):
-        self.spisek_izdelkov.remove(izdelek)
-        self.potrebujem_izdelke.append(izdelek)
-
-    def kupi(self, izdelek):
-        self.potrebujem_izdelke.remove(izdelek)
-        self.spisek_izdelkov.append(izdelek)
+    def stevilo_potrebujem(self):
+        return len(self.potrebujem)
 
 
 class Izdelek:
-    def __init__(self, ime, cena_na_kos, kolicina=0, kupljeno=False):
+    def __init__(self, ime, cena_na_kos, kolicina=0):
         self.ime = ime
-        self.cena = int(cena_na_kos)
+        self.cena = cena_na_kos
         self.kolicina = kolicina
-        self.kupljeno = kupljeno
 
     def __repr__(self):
         return f"Izdelek({self.ime}, {self.cena})"
-
-    def potrebujem(self, kolicina):
-        self.kolicina = kolicina
-
-    def kupi(self):
-        self.kupljeno = True
-
-    def vrni_na_spisek(self):
-        self.kolicina = 0
-        self.kupljeno = False
 
     def strosek_izdelka(self):
         return self.kolicina * self.cena
